0ed2fc5dceb3927ab794f8defd88655afe33c5fb
Joakim Jarsäter <Joakim.Jarsater@postkodlotteriet.se>
2014-04-16 15:56:19 +0200

    test

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "os"
 import "fmt"
 import "sync"
-import "time"
+import "strings"
+import "os/exec"
 import "encoding/json"
 
 type Config struct {
@@ -19,24 +20,44 @@ var config = new(Config)
 
 func main() {
 
+
+	install()
 	setup()
 
+	numSites := len(config.Sites)
+	numSelectors := len(config.Selectors)
+
 	var wg sync.WaitGroup
-	wg.Add(2)
+	wg.Add(numSites * numSelectors)
 
-	cs := make(chan WebSite)
-	defer close(cs)
-
-	for i := 0; i < len(config.Sites); i++ {
-		go fetch(cs, config.Sites[i])
-		go process(cs, &wg, i)
+	for i := 0; i < numSites; i++ {
+		for j := 0; j < numSelectors; j++ {
+			go process(&wg, config.Sites[i], config.Selectors[j])
+		}
 	}
 
 	wg.Wait()
 	resemble()
 }
 
+func install() {
+	cmd := exec.Command("npm", "install")
+	err := cmd.Run()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
 func setup() {
+	cmd1 := exec.Command("rm", "-rf", "screenshots")
+	cmd1.Stdin = strings.NewReader("y")
+	cmd1.Run()
+
+	cmd2 := exec.Command("rm", "-rf", "failures")
+	cmd2.Stdin = strings.NewReader("y")
+	cmd2.Run()
+
 	configFile, err := os.Open("config.json")
 	if err != nil {
 		panic(err)
@@ -46,24 +67,19 @@ func setup() {
 	jsonParser.Decode(&config)
 }
 
-func fetch(cs chan WebSite, site WebSite) {
-	fmt.Println("Fetching: ", site.Url)
+func process(wg *sync.WaitGroup, site WebSite, selector string) {
 
-	// Simulate network lag
-	time.Sleep(time.Second * 4)
-	cs <- site
-}
+	fmt.Println("-- Working ", selector, " at ", site.Url)
+	exec.Command("node_modules/casperjs/bin/casperjs", "test", "lib/a.js",
+			"--url="+site.Url,
+			"--selector="+selector,
+			"--ignore-ssl-errors=yes").Run()
 
-func process(cs chan WebSite, wg *sync.WaitGroup, i int) {
-	s := <-cs
-
-	for j := 0; j < len(config.Selectors); j++ {
-		fmt.Println("-- Working ", config.Selectors[j], " with key ", i, " at ", s.Url);
-	}
 
 	wg.Done()
 }
 
 func resemble() {
 	fmt.Println("Resemble")
+	exec.Command("node_modules/casperjs/bin/casperjs", "test", "lib/b.js").Run()
 }
